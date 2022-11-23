4e955510b1b6f9155ad6da29d1271d276d82ba80
Himanshu Kumar <kumarh664@gmail.com>
2022-11-23 16:51:10 +0530

    implement custom user model and profile model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import NewUser, UserProfile
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+
+class UserAdminConfig(UserAdmin):
+    model = NewUser
+    search_fields = ('email',)
+    list_filter = ('email', 'is_active', 'is_staff', 'is_moderator')
+    ordering = ('-start_date',)
+    inlines = [UserProfileInline]
+    list_display = ('email', 'user_name', 'first_name',
+                    'is_active', 'is_staff', 'is_moderator')
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(NewUser, UserAdminConfig)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseNotFound
+from .models import UserProfile
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+
+
+@login_required
+def ProfileView(request):
+    if request.method == 'POST':
+        user_id = request.user.id
+        age = request.POST['age']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        try:
+            profile = UserProfile.objects.get(id=user_id)
+            profile.age = age
+            profile.first_name = fname
+            profile.last_name = lname
+            profile.save()
+        except ObjectDoesNotExist:
+            UserProfile.objects.create(age=age, user=request.user, first_name=fname, last_name=lname)
+        return redirect('/accounts/profile')
+    else:
+        user_id = request.user.id
+        try:
+            profile = UserProfile.objects.get(id=user_id)
+            return render(request, 'account/profile.html', {'user': request.user, 'profile': profile})
+        except ObjectDoesNotExist:
+            return render(request, 'account/profile.html', {'user': request.user, 'profile': {}})
+            

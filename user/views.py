522a0d88a22380eb3a3a372df7d8f08c7cdfd080
Himanshu Kumar <kumarh664@gmail.com>
2022-12-02 18:50:38 +0530

    fixed profile editor

--- user/views.py
+++ user/views.py
@@ -14,8 +14,32 @@ def ProfileView(request):
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-        return render(request, 'account/profile.html', {'form': ProfileForm()})
+            profile = form.save(commit=False)
+
+            age = profile.age
+            fname = profile.first_name
+            lname = profile.last_name
+            gender = profile.gender
+            pref = profile.preference
+            about = profile.about
+            image = profile.image
+            try:
+                gprofile = UserProfile.objects.get(user=request.user)
+                gprofile.age = age
+                gprofile.first_name = fname
+                gprofile.last_name = lname
+                gprofile.gender = gender
+                gprofile.preference = pref
+                gprofile.about = about
+                gprofile.image = image
+                gprofile.save()
+            except ObjectDoesNotExist:
+                UserProfile.objects.create(age=age, user=request.user, first_name=fname,
+                                           last_name=lname, gender=gender, preference=pref, about=about, image=image)
+        return redirect('/accounts/profile/')
     else:
-        profile = request.user.userprofile
-        return render(request, 'account/profile.html', {'form': ProfileForm(instance=profile)})
+        try:
+            profile = request.user.userprofile
+            return render(request, 'account/profile.html', {'form': ProfileForm(instance=profile)})
+        except:
+            return render(request, 'account/profile.html', {'form': ProfileForm()})
